3b78bf63439148617b1da92610a4678e3dcc850a
Margarita2522 <123013330+Margarita2522@users.noreply.github.com>
2023-02-05 20:57:02 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print(" malenkii joschik, ");
+        System.out.print(" tschetwero noschek, ");
+        System.out.print(" na spine listok nesjot, ");
+        System.out.print(" pesenku pojot ");
+        System.out.println(" ... ");
+        System.out.println(" malenkii joschik,");
+        System.out.println(" tschetwero noschek,");
+        System.out.println(" na spine listok nesjot,");
+        System.out.println(" pesenku pojot...");
+        }
+}
